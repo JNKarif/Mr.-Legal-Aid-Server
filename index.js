0995b1cd385f3a7bd3ccbc60312bb3426da5f594
JNKarif <arif.rahman041@gmail.com>
2022-11-09 15:35:39 +0600

    single-service-api-generated

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
 const app = express();
@@ -22,11 +22,21 @@ async function run() {
     try {
         const serviceCollection = client.db('assignment11').collection('services');
 
+        // creating all services api
         app.get('/services', async (req, res) => {
             const query = {};
             const cursor = serviceCollection.find(query);
             const services = await cursor.toArray();
             res.send(services)
+        });
+
+
+        // creating single service api
+        app.get('/services/:id', async(req, res)=>{
+            const id= req.params.id;
+            const query= {_id: ObjectId(id)};
+            const service= await serviceCollection.findOne(query);
+            res.send(service)
         })
     }
     finally {
