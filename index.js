7169fcbbc23b4b32300aae4929df60f933859ca3
JNKarif <arif.rahman041@gmail.com>
2022-11-09 14:13:53 +0600

    dotenv-userid-password-added

--- index.js
+++ index.js
@@ -17,12 +17,17 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.ei4prfy.mongodb.net/?retryWrites=true&w=majority`;
 // console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
 
+async function run(){
+try{
+const serviceCollection= client.db('assignment11').collection('services')
+}
+finally{
+
+}
+}
+
+run().catch(err=>console.error(err))
 
 app.get('/', (req, res)=>{
     res.send('assignment-11 sever is running')
